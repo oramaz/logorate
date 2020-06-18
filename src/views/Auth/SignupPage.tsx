796413cf6fc9ba59f7801951991a2f9005addd61
oramaz <64165574+oramaz@users.noreply.github.com>
2020-06-18 20:43:01 +0300

    Signup request added

--- src/views/Auth/SignupPage.tsx
+++ src/views/Auth/SignupPage.tsx
@@ -4,9 +4,32 @@ import {
      PwdInputIcon,
 } from "../../assets/images/Icons";
 import {useHistory} from "react-router-dom"
+import axios from "axios";
+import {API_URL} from "../../utils/urls";
 
 export const SignupPage: React.FC = () => {
     const history = useHistory();
+
+   const handleSignup = async (e: any) => {
+      e.preventDefault();
+      const email = e.target.elements.email.value
+      const password = e.target.elements.password.value
+      const password2 = e.target.elements.password2.value
+
+      if(email && password && password === password2) {
+         const res = await axios.get(
+            `${API_URL}/users/register/`,
+            {
+               params: {
+                  email: email,
+                  password: password,
+                  password_confirm: password2
+               }
+            },
+         );
+         console.log(res)
+      }
+   }
 
     return (
         <div className="content-container-1">
@@ -16,18 +39,18 @@ export const SignupPage: React.FC = () => {
                 </div>
             </div>
             <div className="content-form-container mx-auto"  style={{marginBottom: "30px"}}>
-                <form action={`{$API_URL}/user/register`} method={'POST'}>
+                <form onSubmit={(e) => handleSignup(e)} method={'POST'}>
                     <div className="content-input-container" style={{marginBottom: "32px"}}>
                         <span className="input-icon"><EmailInputIcon/></span>
-                        <input type="email" required  className="content-input" placeholder="Email" title="email"/>
+                        <input type="email" name="email" required  className="content-input" placeholder="Email" title="email"/>
                     </div>
                     <div className="content-input-container" style={{marginBottom: "32px"}}>
                         <span className="input-icon"><PwdInputIcon/></span>
-                        <input type="password" required className="content-input" placeholder="Password" />
+                        <input type="password" name="password" required className="content-input" placeholder="Password" />
                     </div>
                     <div className="content-input-container" style={{marginBottom: "50px"}}>
                         <span className="input-icon"><PwdInputIcon/></span>
-                        <input type="password" required className="content-input" placeholder="Confirm the password" />
+                        <input type="password" name="password2" required className="content-input" placeholder="Confirm the password" />
                     </div>
                     <div className="d-flex">
                         <button type="submit" className="content-btn mx-auto">Let's go!</button>
