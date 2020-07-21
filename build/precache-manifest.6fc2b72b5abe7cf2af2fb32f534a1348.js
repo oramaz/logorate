87bd002eca319e9e28520ba57ef37ad475786dae
oramaz <64165574+oramaz@users.noreply.github.com>
2020-07-21 22:34:02 +0300

    build

---
+++ build/precache-manifest.6fc2b72b5abe7cf2af2fb32f534a1348.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "086a5cbbcaf4c84233f1cfb763b787fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4facd9688bff9001e992",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2999bcf025e1ecc88bb4",
+    "url": "/static/css/main.5968eeee.chunk.css"
+  },
+  {
+    "revision": "4facd9688bff9001e992",
+    "url": "/static/js/2.43bb1307.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.43bb1307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2999bcf025e1ecc88bb4",
+    "url": "/static/js/main.62e91c6b.chunk.js"
+  },
+  {
+    "revision": "bfa77427352be2c07e0e",
+    "url": "/static/js/runtime-main.4a715ead.js"
+  },
+  {
+    "revision": "4ca2db119ca6198a1437627e44f71bc9",
+    "url": "/static/media/banner.4ca2db11.jpg"
+  },
+  {
+    "revision": "6d63096233ad70bd9eb6b695e616e89a",
+    "url": "/static/media/banner.6d630962.webp"
+  }
+]);
